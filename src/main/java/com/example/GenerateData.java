47abe1b3e0a4d27741028c5b0d8c8a0f31cbc6a3
Livia-11 <kirezilivia@gmail.com>
2025-01-23 08:13:50 +0200

    Adding GenerateData file

---
+++ src/main/java/com/example/GenerateData.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GenerateData {
+}
